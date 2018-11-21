a68a398ccc47b6fc012d3f7ab16b3f4c27bd33e8
George Daramouskas <gdaramouskas@therp.nl>
2018-11-21 17:32:31 +0100

    [MIG] l10n_nl_partner_name: Migration to 11.0

--- l10n_nl_partner_name/model/res_partner.py
+++ l10n_nl_partner_name/model/res_partner.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Therp BV <https://therp.nl>
+# Copyright 2017 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from mako.template import Template
 from odoo import models, fields, api
